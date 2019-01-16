56c67d13d5b3a56cc746e03909e833e4853cfbd1
Isaiah Santillan <isaiahsantillan111@icloud.com>
2019-01-16 14:37:43 -0500

    updated my html and css

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
+import csv
 
 from flask import Flask
 from flask import request
+from flask import render_template
+
+
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
+   return render_template('index.html')
